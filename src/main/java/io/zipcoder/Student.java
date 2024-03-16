26eff2db0966171eb9aaa1902cfea344c5148bfc
MichaelS0521 <michael@Zipcoders-MacBook-Pro-4.local>
2024-03-16 09:47:01 -0400

    Updated

--- src/main/java/io/zipcoder/Student.java
+++ src/main/java/io/zipcoder/Student.java
@@ -1,4 +1,40 @@
 package io.zipcoder;
 
+import java.util.ArrayList;
+
 public class Student {
+    String firstName;
+    String lastName;
+    ArrayList<Double> examScores;
+
+    public Student(String firstName, String lastName, ArrayList<Double> examScores) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.examScores = examScores;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getNumberOfExamsTaken() {
+        int numExamsTaken = 0;
+        for (int i = 0; i < examScores.size(); i++){
+            numExamsTaken++;
+        }
+        return numExamsTaken;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 }
